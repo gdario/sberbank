35b8573147f1df39b7145a5f0fc47824dcdbabf9
Giovanni d'Ario <giovanni.dario@gmail.com>
2017-05-11 07:51:20 +0200

    Continue clearning the training set.

--- R/clean_training_set.R
+++ R/clean_training_set.R
@@ -10,8 +10,21 @@ train$week <- as.integer(1 + trunc(
   (train$timestamp - starting_time) / 7
 ))
 
-### The columns from max_floor to kitch_sq should be numeric
-### NOTE build_year and kitch_sq contain entry mistakees
+### Time variables
+train %<>% mutate(
+  year = lubridate::year(timestamp),
+  month = lubridate::month(timestamp)
+)
+
+### Demographic variables
+train %<>% mutate(
+  popul_dens_raion = raion_popul / area_m, # Population density
+  full_dens = full_all / area_m,
+  young_dens = young_all / area_m,
+  work_dens = work_all / area_m
+)
+
+### Building/flat specific variables. 
 train %<>% mutate(
   max_floor = as.integer(max_floor),
   build_year = as.integer(build_year),
@@ -33,6 +46,20 @@ train %<>% mutate(
   num_room = ifelse(num_room == 0, NA, num_room),
   floor = ifelse(floor > max_floor, NA, floor)
 )
+
+# Take a look at the number of transactions by sub_area by year
+# transactions_per_year <- train %>% 
+#   group_by(year, sub_area) %>% 
+#   summarise(n_transactions = n())
+# 
+# ggplot(
+#   data = tmp, 
+#   aes(x = reorder(sub_area, n_transactions), 
+#     y = n_transactions)) + 
+#   geom_bar(stat = "identity") + 
+#   facet_wrap(~ year) + 
+#   theme(axis.text.x = element_text(angle = 90, vjust = 1, hjust = 1)
+#   )
 
 # ### Drop the following two columns, as they have very low variance,
 # ### and they are difficult to interpret
